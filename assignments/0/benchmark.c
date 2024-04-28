fee702e9df64225e6dda7894f66a56009cf5223e
pgurtner <81441242+pgurtner@users.noreply.github.com>
2024-04-28 11:54:23 +0200

    initial commit

---
+++ assignments/0/benchmark.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+
+double approximate_pi () {
+	int SLICES = 1000000000;
+	double sum=0.;
+	double delta_x = 1.0/SLICES;
+
+	for (int i=0; i < SLICES; i++) {
+  	double x = (i+0.5)*delta_x;
+  	sum += 4.0 * sqrt(1.0 - x * x);
+	}
+	double Pi = sum * delta_x;
+
+	return Pi;
+}
+
+int main (void) {
+	double pi = approximate_pi();
+	printf("%f\n", pi);
+	return 0;
+}
